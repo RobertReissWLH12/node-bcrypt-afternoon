9f857c8c7c21f0ed8923260210b1857c918898f6
Robert Reiss <reissaudiodesign@yahoo.com>
2019-11-12 19:38:01 -0700

    got nodemon running; completed through step 4

---
+++ server/index.js
@@ -0,0 +1,29 @@
+require('dotenv').config()
+const express = require('express')
+const massive = require('massive')
+const session = require('express-session')
+const {CONNECTION_STRING, SESSION_SECRET} = process.env
+const authCtrl = require('./controllers/authController')
+
+const app = express();
+const PORT = 4000;
+
+// TOP-LEVEL MIDDLEWARE
+app.use(express.json())
+
+
+app.use(session({
+    resave: true,
+    saveUninitialized: false,
+    secret: SESSION_SECRET
+}))
+
+app.post('/auth/register', authCtrl.register)
+// app.post('/auth/login', authCtrl.login)
+// app.get('/auth/logout', authCtrl.logout)
+
+massive(CONNECTION_STRING).then(db => {
+    app.set('db', db)
+    console.log('db connected')
+})
+app.listen(PORT, () => console.log(`I am fluent in over ${PORT} forms of communication.`))
